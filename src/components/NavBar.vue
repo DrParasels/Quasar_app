3ece278df7e9da686e48cba7a8d283d6eeaf2e79
DrParasels <fomishov121@yandex.ru>
2022-10-28 16:43:36 +0300

    make menubar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,13 @@
+<template>
+  <router-link to="/">Главная</router-link>
+  <router-link to="/day">Распорядок дня</router-link>
+  <router-link to="/week">Распорядок на неделю</router-link>
+</template>
+
+<style></style>
+
+<script>
+export default {
+  name: "NavBar",
+};
+</script>
---
+++ src/layouts/MyLayout.vue
@@ -0,0 +1,35 @@
+<template>
+  <q-layout view="hHh Lpr fFf">
+    <!-- Be sure to play with the Layout demo on docs -->
+
+    <!-- (Optional) The Header -->
+    <q-header elevated>
+      <q-toolbar class="row justify-center">
+        <q-tabs v-model="tab">
+          <q-route-tab name="Main" label="Главная" to="/" />
+          <q-route-tab name="Day" label="Распорядок на день" to="/day" />
+          <q-route-tab name="Week" label="Распорядок на неделю" to="/week" />
+        </q-tabs>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <!-- This is where pages get injected -->
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { ref } from "vue";
+export default {
+  setup() {
+    return {
+      leftDrawer: false,
+      tab: ref(""),
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
